d3db411750e0be2f6721a13ed25dbc64ac8781ee
Enes Berk Yetim <110656328+enesberkyetim@users.noreply.github.com>
2024-11-29 01:01:39 +0300

    ..

---
+++ cmake-build-debug/CMakeFiles/AssemblerForProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "AssemblerForProject.exe"
+  "AssemblerForProject.exe.manifest"
+  "AssemblerForProject.pdb"
+  "CMakeFiles/AssemblerForProject.dir/main.c.obj"
+  "CMakeFiles/AssemblerForProject.dir/main.c.obj.d"
+  "libAssemblerForProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AssemblerForProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -1,11 +1,84 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 int main(void) {
-    FILE *input_file = fopen("input.txt", "r");
+    FILE *input_file = fopen("input.txt", "r+");
 
-    while (fgetc(input_file) != EOF) {
+    while (feof(input_file) == 0) {
+        char line[100];
+        fgets(line, 100, input_file);
+        char opcode_str[6];
 
+        int i = 0;
+
+        while (line[i] != ' ') {
+            opcode_str[i] = line[i];
+            i++;
+        }
+
+        opcode_str[i] = '\0';
+        i++;
+
+        if (strcmp(opcode_str, "ADD") == 0) {
+            printf("ADD\n");
+        }
+        else if (strcmp(opcode_str, "ADDI") == 0) {
+            printf("ADDI\n");
+        }
+        else if (strcmp(opcode_str, "NAND") == 0) {
+            printf("NAND\n");
+        }
+        else if (strcmp(opcode_str, "NANDI") == 0) {
+            printf("NANDI\n");
+        }
+        else if (strcmp(opcode_str, "OR") == 0) {
+            printf("OR\n");
+        }
+        else if (strcmp(opcode_str, "ORI") == 0) {
+            printf("ORI\n");
+        }
+        else if (strcmp(opcode_str, "SUB") == 0) {
+            printf("SUB\n");
+        }
+        else if (strcmp(opcode_str, "SUBI") == 0) {
+            printf("SUBI\n");
+        }
+        else if (strcmp(opcode_str, "SLL") == 0) {
+            printf("SLL\n");
+        }
+        else if (strcmp(opcode_str, "SLLI") == 0) {
+            printf("SLLI\n");
+        }
+        else if (strcmp(opcode_str, "LD") == 0) {
+            printf("LD\n");
+        }
+        else if (strcmp(opcode_str, "ST") == 0) {
+            printf("ST\n");
+        }
+        else if (strcmp(opcode_str, "JUMP") == 0) {
+            printf("JUMP\n");
+        }
+        else if (strcmp(opcode_str, "BEQ") == 0) {
+            printf("BEQ\n");
+        }
+        else if (strcmp(opcode_str, "BLT") == 0) {
+            printf("BLT\n");
+        }
+        else if (strcmp(opcode_str, "BGT") == 0) {
+            printf("BGT\n");
+        }
+        else if (strcmp(opcode_str, "BLE") == 0) {
+            printf("BLE\n");
+        }
+        else if (strcmp(opcode_str, "BNE") == 0) {
+            printf("BNE\n");
+        }
+
+        for (int j = 0; j < 5; j++) {
+            opcode_str[j] = '\0';
+        }
     }
     return 0;
 }
